6671090d2c06d8452e797b714207bd664a482130
luiz.taira@fluig.com <luiz.taira@fluig.com>
2016-10-13 13:55:21 -0300

    commit inicial

---
+++ src/main/java/com/luiztaira/m101j/App.java
@@ -0,0 +1,13 @@
+package com.luiztaira.m101j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
