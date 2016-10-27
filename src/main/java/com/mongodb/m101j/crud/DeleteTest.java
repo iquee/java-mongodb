1aaa8dde3c3dca4223b906faf37f34e33f00db5b
“iquee” <luizcomz@gmail.com>
2016-10-26 22:09:51 -0200

    alteracao do delete

--- src/main/java/com/mongodb/m101j/crud/DeleteTest.java
+++ src/main/java/com/mongodb/m101j/crud/DeleteTest.java
@@ -17,33 +17,33 @@
 package com.mongodb.m101j.crud;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
-import java.util.ArrayList;
-
-import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Filters.gt;
-import static com.mongodb.client.model.Filters.gte;
-import static com.mongodb.m101j.util.Helpers.printJson;
-
 public class DeleteTest {
     public static void main(String[] args) {
         MongoClient client = new MongoClient();
-        MongoDatabase database = client.getDatabase("course");
-        MongoCollection<Document> collection = database.getCollection("test");
+        MongoDatabase database = client.getDatabase("students");
+        MongoCollection<Document> collection = database.getCollection("grades");
 
-        collection.drop();
+        // collection.drop();
+        FindIterable<Document> sort = collection.find(
+                new Document("type", "homework"))
+                .sort(new Document("student_id", 1)
+                        .append("score", 1));
 
-        // insert 8 documents, with _id set to the value of the loop variable
-        for (int i = 0; i < 8; i++) {
-            collection.insertOne(new Document().append("_id", i));
+        Integer idToRemove = Integer.MIN_VALUE;
+        for (Document doc : sort) {
+            int id = doc.getInteger("student_id");
+            if (id != idToRemove){
+                collection.deleteOne(doc);
+                idToRemove = doc.getInteger("student_id");
+                System.out.println("removendo: " + doc);
+            } else{
+                System.out.println("sobrando: " + doc);
+            }
         }
-
-        collection.deleteOne(eq("_id", 4));
-
-        for (Document cur : collection.find().into(new ArrayList<Document>())) {
-            printJson(cur);
-        }
-    }}
+    }
+}
